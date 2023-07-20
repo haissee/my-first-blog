df7b428b56ceba040fac51f3f3f64b9441fcb7d0
Your Name <you@example.com>
2023-07-20 14:12:32 +0200

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+
 def django_countries(request):
-    return render(request, 'blog/django_countries.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/django_countries.html', {'posts': posts})
